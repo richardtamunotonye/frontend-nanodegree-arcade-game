16bfcab089d331654d29bc82116071ec0effacab
HELLO WORLD!!! <HELLO WORLD!!!>
2017-05-23 08:23:34 +0200

    Added Score and Lives

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function() {
+    "use strict";
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    this.x = -101;
+    this.y = 0;
+
+    this.maxSpeed = 300;
+    // maximum speed applicable
+
+    // resolve the enemy's row position
+     this.y = (Math.floor(Math.random() * 4) * 83) + 83;
+
+     // resolve speed of an enemy
+    this.speed = Math.floor(Math.random() * this.maxSpeed + 1);
+
+    this.playerSlay = 0;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
+
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
@@ -19,103 +29,127 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.speed * dt;
 
-    // when off canvas, reset position of enemy to move across again
-    if (this.x > 550) {
-        this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 512);
-    }
-
     // Check for collision between player and enemies
-    if (player.x < this.x + 60 &&
-        player.x + 37 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
-        player.x = 200;
-        player.y = 380;
-    }
+    if ((Math.abs(this.x - player.x) < 70) && (Math.abs(this.y - player.y) < 15)) {
+    player.reset();
+    this.playerSlay++;
+  }
+
+    // when off canvas, reset position of enemy
+    if (this.x > ctx.canvas.width) {
+    this.x = -101;
+    this.y = (Math.floor(Math.random() * 4) * 83) + 83;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/char-boy.png';
+
+//Player Class
+var Player = function() {
+    "use strict";
+
+    this.x = 404;
+    this.y = 404;
+    this.sprite = "images/char-cat-girl.png";
+    this.score = 0;
+    this.lives = 3;
 };
 
-Player.prototype.update = function() {
-    // Prevent player from moving beyond canvas wall boundaries
-    if (this.y > 380) {
-        this.y = 380;
-    }
 
-    if (this.x > 400) {
-        this.x = 400;
-    }
-
-    if (this.x < 0) {
-        this.x = 0;
-    }
-
-    // Check for player reaching top of canvas and winning the game
-    if (this.y < 0) {
-        this.x = 200;
-        this.y = 380;
-    }
+Player.prototype.returnToStart = function() {
+  this.y = 404;
 };
 
+Player.prototype.reset = function() {
+  this.lives--;
+  this.returnToStart();
+};
+
+Player.prototype.hitPoint = function() {
+  this.score++;
+  this.returnToStart();
+  // increase enemy speed
+  allEnemies.forEach(function(enemy) {
+    enemy.speed += 15;
+  });
+  if (this.score % 3 === 0) {
+    // player gets a new life after every three point
+    this.lives++;
+    // and a new enemy
+    allEnemies.push(new Enemy());
+  }
+  // update score on screen
+};
+
+//Update Function
+Player.prototype.update = function(dt) {
+  if (this.y < 0) {
+    this.hitPoint();
+  }
+  };
+
+// Player.render()
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+  ctx.font = "26px Arial";
+  ctx.fillStyle = "#000";
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#000";
+  ctx.fillText("Score: " + this.score, 405, 50);
+  ctx.strokeText("Score: " + this.score, 405, 50);
+  ctx.fillText("Lives: " + this.lives, 205, 50);
+  ctx.strokeText("Lives: " + this.lives, 205, 50);
+
+
+};
+//handleInput function
+Player.prototype.handleInput = function(eventKey) {
+  var nonGameKey = false;
+  switch (eventKey) {
+    case "up":
+      this.y -= 83;
+      // this.hitPoint() will prevent the
+      // player from going too far up
+      break;
+    case "down":
+      if (this.y < 404) {
+        this.y += 83;
+      }
+      break;
+    case "left":
+      if (this.x > 0) {
+        this.x -= 101;
+      }
+      break;
+    case "right":
+      if (this.x < 808) {
+        this.x += 101;
+      }
+      break;
+    default:
+      // the user pressed some other key
+      nonGameKey = true;
+  }
 };
 
-Player.prototype.handleInput = function(keyPress) {
-    switch (keyPress) {
-        case 'left':
-            this.x -= this.speed + 50;
-            break;
-        case 'up':
-            this.y -= this.speed + 30;
-            break;
-        case 'right':
-            this.x += this.speed + 50;
-            break;
-        case 'down':
-            this.y += this.speed + 30;
-            break;
-    }
-};
+// instantiating the objects.
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(),
+  new Enemy(), new Enemy(), new Enemy()
+];
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [];
+var player = new Player();
 
-// Position "y" where the enemies will are created
-var enemyPosition = [60, 140, 220];
-var player = new Player(200, 380, 50);
-var enemy;
-
-enemyPosition.forEach(function(posY) {
-    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
-    allEnemies.push(enemy);
-});
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
